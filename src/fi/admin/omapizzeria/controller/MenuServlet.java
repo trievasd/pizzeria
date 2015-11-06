17658633d807f689cb10e74fc84d6a2bb7c5e5f0
Veikko Soosaar <veixer@hotmail.com>
2015-11-06 13:27:37 +0200

    menuServlet

---
+++ src/fi/admin/omapizzeria/controller/MenuServlet.java
@@ -0,0 +1,54 @@
+package fi.admin.omapizzeria.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.PizzaDAO;
+import fi.omapizzeria.admin.bean.Pizza;
+
+/**
+ * Servlet implementation class MenuServlet
+ */
+@WebServlet("/menu")
+public class MenuServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MenuServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		PizzaDAO pDao = new PizzaDAO();
+		List<Pizza> pizzat = pDao.haePizzat();	
+		
+		
+		
+		
+		request.setAttribute("pizzat", pizzat);
+		
+		request.getRequestDispatcher("menu.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
